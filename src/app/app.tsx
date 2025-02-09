82b591e3a38476cf72cbd2dbd4a9a78582479200
Dkey51 <oniblekir@gmail.com>
2025-02-09 16:30:46 +0300

    переименовал файлы, добавил использование компонента header

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,12 +2,13 @@ import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import { AppRoute, AuthorizationStatus } from '../const';
 import PrivateRoute from '../components/private-route/private-route';
+
 //Импортируем главный экран
 import Main from '../pages/main/main';
 
 // Динамически импортируем остальные экраны
-const NotFoundScreenPreview = lazy(() => import ('../pages/notFoundScreen/pageNotFound')) ;
-const AuthScreenPreview = lazy(() => import ('../pages/login/authScreen')) ;
+const NotFoundScreenPreview = lazy(() => import ('../pages/page-not-found/page-not-found')) ;
+const AuthScreenPreview = lazy(() => import ('../pages/auth-screen/auth-screen')) ;
 const FavoritesEmptyPreview = lazy(() => import ('../pages/favorites/favorites-empty')) ;
 const FavoritesPreview = lazy(() => import ('../pages/favorites/favorites')) ;
 const OfferScreenPreview = lazy(() => import ('../pages/offer/offer')) ;
