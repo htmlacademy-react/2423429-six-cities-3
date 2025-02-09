429a9d44e7ed97c9748b51a008cb82ce7bc88153
Dkey51 <oniblekir@gmail.com>
2025-02-09 16:03:28 +0300

    обернул экраны в lazy

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,17 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import { AppRoute } from '../const';
 
+//Импортируем главный экран
 import Main from '../pages/main/main';
-import NotFoundScreen from '../pages/notFoundScreen/pageNotFound';
-import AuthScreen from '../pages/login/authScreen';
-import FavoritesEmpty from '../pages/favorites/favorites-empty';
-import Favorites from '../pages/favorites/favorites';
-import OfferScreen from '../pages/offer/offer';
+
+// Динамически импортируем остальные экраны
+const NotFoundScreenPreview = lazy(() => import ('../pages/notFoundScreen/pageNotFound')) ;
+const AuthScreenPreview = lazy(() => import ('../pages/login/authScreen')) ;
+const FavoritesEmptyPreview = lazy(() => import ('../pages/favorites/favorites-empty')) ;
+const FavoritesPreview = lazy(() => import ('../pages/favorites/favorites')) ;
+const OfferScreenPreview = lazy(() => import ('../pages/offer/offer')) ;
+
 
 type AppScreenProps = {
   placesCount: number;
@@ -15,32 +20,34 @@ type AppScreenProps = {
 function App({placesCount}: AppScreenProps): JSX.Element {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path={AppRoute.Root}
-          element={<Main placesCount={placesCount} />}
-        />
-        <Route
-          path={AppRoute.Login}
-          element={<AuthScreen />}
-        />
-        <Route
-          path={AppRoute.Favorites}
-          element = {<Favorites />}
-        />
-        <Route
-          path={AppRoute.FavoritesEmpty}
-          element = {<FavoritesEmpty />}
-        />
-        <Route
-          path={AppRoute.Offer}
-          element={<OfferScreen />}
-        />
-        <Route
-          path="*"
-          element={<NotFoundScreen />}
-        />
-      </Routes>
+      <Suspense fallback={<div>Loading....</div>}>
+        <Routes>
+          <Route
+            path={AppRoute.Root}
+            element={<Main placesCount={placesCount} />}
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<AuthScreenPreview />}
+          />
+          <Route
+            path={AppRoute.Favorites}
+            element = {<FavoritesPreview />}
+          />
+          <Route
+            path={AppRoute.FavoritesEmpty}
+            element = {<FavoritesEmptyPreview />}
+          />
+          <Route
+            path={AppRoute.Offer}
+            element={<OfferScreenPreview />}
+          />
+          <Route
+            path="*"
+            element={<NotFoundScreenPreview />}
+          />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
