69fc4243022ce205ed7e72e924feaf7628504fb9
Kirill Astramovich <oniblekir@gmail.com>
2025-03-05 16:34:25 +0300

    изменил условие с тернарного оператора на логическую проверку через &&

--- src/components/places-card.tsx
+++ src/components/places-card.tsx
@@ -9,10 +9,12 @@ function PlacesCard({placeOffer}: PlacesCardProps): JSX.Element {
 
   return (
     <article className="cities__card place-card">
-      {placeOffer.isPremium ? (
-    <div className="place-card__mark">
-      <span>Premium</span>
-    </div>) : ''}
+      {placeOffer.isPremium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+        )
+      }
     <div className="cities__image-wrapper place-card__image-wrapper">
       <a href="#">
         <img className="place-card__image" src={placeOffer.previewImage} width="260" height="200" alt="Place image" />
