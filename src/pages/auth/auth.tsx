06b1c0cd984407c61ac03e47b1b676f302cdd5fa
Dkey51 <oniblekir@gmail.com>
2025-05-16 00:38:30 +0300

    7.16. Обновит компонент Auth.
    Пора «оживить» компонент «Auth».
    Пользователь может попасть на
    этот экран двумя способами:
    
    1. Самостоятельно перейти по маршруту `/login`. Для выполнения
    авторизации в самом начале.
    
    На этом экране пользователь может ввести логин и пароль
    
    Воспользуемся хуком `useRef` для получения доступа к элементам формы.
    Нам потребуется взаимодействовать с хранилищем (диспатчить действия),
    поэтому подключим компонент к нему.

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -1,6 +1,30 @@
 import Logo from '../../components/logo/logo';
+import { FormEvent, useRef } from 'react';
+import { useAppDispatch } from '../../hooks';
+import { useNavigate } from 'react-router-dom';
+import { loginAction } from '../../store/api-actions';
+import { AppRoute } from '../../const/const';
 
 function AuthScreen(): JSX.Element {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      dispatch(
+        loginAction({
+          login: loginRef.current.value,
+          password: passwordRef.current.value,
+        })
+      );
+    }
+  };
+
   return (
     <div className="page page--gray page--login">
       <header className="header">
@@ -17,10 +41,16 @@ function AuthScreen(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form
+              className="login__form form"
+              action="#"
+              onSubmit={handleSubmit}
+              method="post"
+            >
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
                 <input
+                  ref={loginRef}
                   className="login__input form__input"
                   type="email"
                   name="email"
@@ -31,6 +61,7 @@ function AuthScreen(): JSX.Element {
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
                 <input
+                  ref={passwordRef}
                   className="login__input form__input"
                   type="password"
                   name="password"
@@ -39,6 +70,7 @@ function AuthScreen(): JSX.Element {
                 />
               </div>
               <button
+                onClick={() => navigate(AppRoute.Favorites)}
                 className="login__submit form__submit button"
                 type="submit"
               >
@@ -58,5 +90,3 @@ function AuthScreen(): JSX.Element {
     </div>
   );
 }
-
-export default AuthScreen;
