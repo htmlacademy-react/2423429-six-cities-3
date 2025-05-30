eab45a524e65db0841e56902cb441470bbf47fa2
Kirill Astramovich <oniblekir@gmail.com>
2025-05-30 11:46:36 +0300

    пустые строки

--- src/store/favorites/selectors.ts
+++ src/store/favorites/selectors.ts
@@ -4,5 +4,3 @@ export const getFavorites = (state: RootState) => state.favorites.favorites;
 export const getFavoritesLoadingStatus = (state: RootState) => state.favorites.isLoadingFavorites;
 export const getFavoritesError = (state: RootState) => state.favorites.errorFavorites;
 export const getChangingFavoriteStatus = (state: RootState) => state.favorites.isChangingFavoriteStatus;
-
-
